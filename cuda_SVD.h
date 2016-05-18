cdd75c5552d4870d221415e707e27e9fc0fab2c6
lx9t01 <alex.xuhaixiang@gmail.com>
2016-05-18 16:57:18 -0700

    h file

---
+++ cuda_SVD.h
@@ -0,0 +1,56 @@
+// cuda_SVD.h
+
+#ifndef _INCL_GUARD
+
+#define _INCL_GUARD
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include <Eigen/Dense>
+
+
+/*
+gpuErrChk
+Modified from:
+http://stackoverflow.com/questions/14038589/
+what-is-the-canonical-way-to-check-for-errors-using-the-cuda-runtime-api
+*/
+/*
+#define gpuErrChk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(
+    cudaError_t code,
+    const char *file,
+    int line,
+    bool abort=true)
+{
+    if (code != cudaSuccess) {
+        fprintf(stderr,"GPUassert: %s %s %d\n",
+            cudaGetErrorString(code), file, line);
+        exit(code);
+    }
+}
+
+cudaEvent_t start;
+cudaEvent_t stop;
+
+#define START_TIMER() {                         \
+    gpuErrChk(cudaEventCreate(&start));         \
+    gpuErrChk(cudaEventCreate(&stop));          \
+    gpuErrChk(cudaEventRecord(start));          \
+}
+
+#define STOP_RECORD_TIMER(name) {                           \
+    gpuErrChk(cudaEventRecord(stop));                       \
+    gpuErrChk(cudaEventSynchronize(stop));                  \
+    gpuErrChk(cudaEventElapsedTime(&name, start, stop));    \
+    gpuErrChk(cudaEventDestroy(start));                     \
+    gpuErrChk(cudaEventDestroy(stop));                      \
+}
+*/
+
+#endif
