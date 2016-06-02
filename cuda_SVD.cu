c984ffd059a5f582114544c1b60ce3ddb790335e
lx9t01 <alex.xuhaixiang@gmail.com>
2016-06-02 03:32:28 -0700

    printf

--- cuda_SVD.cu
+++ cuda_SVD.cu
@@ -38,6 +38,7 @@ void cudaMultiplyKernel(
         for (int i = 0; i < num_f; ++i) {
             dev_R1[thread_idx] += dev_P[row * num_f + i] * dev_Q[i * num_items + col];
             // might need atomic add?
+            printf("%f\n", dev_R1[thread_idx]);
         }
         thread_idx += blockDim.x * gridDim.x;
     }
